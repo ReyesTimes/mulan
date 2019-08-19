eb5f30e4d981b4601b94e994415d7fc1eddb0233
juan.reyes <juan.reyes@sngular.com>
2019-08-19 13:50:20 -0500

    add nodemail

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 const app = express();
+
+require('dotenv').config();
 
 app.use(express.json());
 
 app.get('/', function (req, res) {
     const transporter = nodemailer.createTransport({
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail'
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        }
     });
 
     transporter.sendMail({
-        from: 'juan.reyes@sngular.com',
-        to: 'rated.mvp1@gmail.com',
+        from: 'rated.mvp1@gmail.com',
+        to: 'juan.reyes@sngular.com',
         subject: 'New contact form message', 
         text: 'blablabla'
+    }, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email send', data);
+        }
     });
 
     res.status(200).send({ 'message': 'OH YEAH GET' })
