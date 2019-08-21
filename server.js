b7d0364b3212492a46e2ff48afd71c43fd23a174
juan.reyes <juan.reyes@sngular.com>
2019-08-21 12:03:10 -0500

    check service

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const xoauth2 = require('xoauth2');
+const validator = require('validator');
+const xssFilters = require('xss-filters');
+const bodyParser = require('body-parser')
 const app = express();
 
 require('dotenv').config();
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    const transporter = nodemailer.createTransport({
+const rejectFunctions = new Map([
+    [ 'name', v => v.length < 5 ],
+    [ 'email', v => !validator.isEmail(v) ],
+    [ 'telephone', v => v.length < 5 ],
+    [ 'description', v => v.length < 5 ]
+])
+
+const validateAndSanitize = (key, value) => {
+    // If map has key and function returns false, return sanitized input. Else, return false
+    return rejectFunctions.has(key) && !rejectFunctions.get(key)(value) && xssFilters.inHTMLData(value)
+}
+
+/*app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});*/
+
+app.post('/', function (req, res) {
+    const attributes = ['name', 'email', 'description', 'telephone'];
+
+    // Map each attribute name to the validated and sanitized equivalent (false if validation failed)
+    const sanitizedAttributes = attributes.map(n => validateAndSanitize(n, req.body[n]))
+
+    // True if some of the attributes new values are false -> validation failed
+    const someInvalid = sanitizedAttributes.some(r => !r)
+
+    console.log(someInvalid, ':D');
+    /*const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             type: 'OAuth2',
@@ -19,12 +49,11 @@ app.get('/', function (req, res) {
             accessToken: process.env.ACCESSTOKEN,
         }
     });
-    //1/tENQHJvujRHTNo8r5pwHyJl7aQEJ1GoQzNvp-mse8O8
 
     transporter.sendMail({
         from: 'rated.mvp1@gmail.com',
-        to: 'juan.reyes@sngular.com',
-        subject: 'New contact form message', 
+        to: 'contacto@ssrefiscales.com.mx',
+        subject: 'Nuevo mensaje de contacto', 
         text: 'blablabla'
     }, function (err, data) {
         if (err) {
@@ -32,7 +61,7 @@ app.get('/', function (req, res) {
         } else {
             console.log('Email send', data);
         }
-    });
+    });*/
 
     res.status(200).send({ 'message': 'OH YEAH GET' })
 });
